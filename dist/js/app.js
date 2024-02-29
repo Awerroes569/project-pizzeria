cd36952739206e6ba04651f510d2a48ea80191c1
Awerroes <arssoo569@gmail.com>
2024-02-29 12:36:35 +0100

    implementing changes according to kodilla instructions

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,13 +1,56 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 
 const app = {
 
+  activatePage: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(
+        classNames.pages.active,
+        page.id == pageId
+      );
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+    
+  },
+  
+  initPages: function () {
+    const thisApp = this;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = idFromHash;
+        break;
+      }
+    }
+    
+    thisApp.activatePage(pageMatchingHash );//thisApp.pages[0].id);
+    console.log('thisApp.links:', thisApp.links);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const id = link.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  
   initMenu: function () {
     const thisApp = this;
-    //console.log('thisApp.data:', thisApp.data);
     for (let productData in thisApp.data.products) {
       new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
     }
@@ -43,8 +86,12 @@ const app = {
   init: function(){
     const thisApp = this;
     console.log('*** App starting ***');
+    thisApp.initPages();
+    console.log('*** App initPages ***');
     thisApp.initData();
+    console.log('*** App initData ***');
     thisApp.initCart();
+    console.log('*** App initCart ***');
   },
 };
 
--- dist/js/settings.js
+++ dist/js/settings.js
@@ -2,11 +2,14 @@ export const select = {
     templateOf: {
         menuProduct: "#template-menu-product",
         cartProduct: '#template-cart-product',
+        bookingWidget: '#template-booking-widget',
     },
     containerOf: {
         menu: '#product-list',
         cart: '#cart',
         cartList: '.cart__order-summary',
+        pages: '#pages',
+        booking: '.booking-wrapper',
     },
     all: {
         menuProducts: '#product-list > .product',
@@ -27,7 +30,26 @@ export const select = {
             linkDecrease: 'a[href="#less"]',
             linkIncrease: 'a[href="#more"]',
         },
+        datePicker: {
+            wrapper: '.date-picker',
+            input: `input[name="date"]`,
+        },
+        hourPicker: {
+            wrapper: '.hour-picker',
+            input: 'input[type="range"]',
+            output: '.output',
+        },
     },
+
+    booking: {
+        peopleAmount: '.people-amount',
+        hoursAmount: '.hours-amount',
+        tables: '.floor-plan .table',
+    },
+    nav: {
+        links: '.main-nav a',
+    },
+
     cart: {
         //toggleTrigger: '.cart__summary',
         //productList: '.cart__order-summary',
@@ -62,6 +84,17 @@ export const classNames = {
     cart: {
         wrapperActive: 'active',
     },
+
+    booking: {
+        loading: 'loading',
+        tableBooked: 'booked',
+    },
+    nav: {
+        active: 'active',
+    },
+    pages: {
+        active: 'active',
+    }
 };
 
 export const settings = {
@@ -69,20 +102,35 @@ export const settings = {
         defaultValue: 1,
         defaultMin: 1,
         defaultMax: 9,
-    }, // CODE CHANGED
-    // CODE ADDED START
+    }, 
     cart: {
         defaultDeliveryFee: 20,
     },
-    // CODE ADDED END
     db: {
         url: '//localhost:3131',
         products: 'products',
         orders: 'orders',
-    }
+        bookings: 'bookings',
+        events: 'events',
+        dateStartParamKey: 'date_gte',
+        dateEndParamKey: 'date_lte',
+        notRepeatParam: 'repeat=false',
+        repeatParam: 'repeat_ne=false',
+    },
+    hours: {
+        open: 12,
+        close: 24,
+    },
+    datePicker: {
+        maxDaysInFuture: 14,
+    },
+    booking: {
+        tableIdAttribute: 'data-table',
+    },
 };
 
 export const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
     cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
